a8d73b86037bb748e413d83c396f404b45495ed1
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-02-01 18:55:45 +0530

    Update Q-8.c

--- Q-8.c
+++ Q-8.c
@@ -39,3 +39,22 @@ int main()
  12321
 1234321
 */
+
+by using ternory operators:
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=4;i++)
+    {k=0;//important !
+        for(j=1;j<=7;j++)
+        {
+        (j>=5-i && j<=3+i)?(j<=4)?printf("%d",++k):printf("%d",--k):printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
