b1ff94fa264fa4e31aa00c00a11bee6c7295a90d
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-02-01 20:58:09 +0530

    Update Q-18.c

--- Q-18.c
+++ Q-18.c
@@ -34,7 +34,7 @@ int main()
 
     return 0;
 }
-/*
+
          *
 
        * * *
@@ -52,4 +52,21 @@ int main()
        * * *
 
          *
- */
+ 
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=9;i++)
+    {
+        for(j=1;j<=9;j++)
+        {
+    (i<=5)?(j>=6-i && j<=4+i)?printf(" *"): printf("  "):(j>=i-4 && j<=14-i)?printf(" *"):printf("  ");
+        }
+        printf("\n\n");
+    }
+
+    return 0;
+ 
+}
